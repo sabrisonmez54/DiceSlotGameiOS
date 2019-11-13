b3b50a4e1adb8f465da00ab134336e800a357730
Sabri <sabri.sonmez@macaulay.cuny.edu>
2019-11-13 13:02:17 -0500

    Initial Commit

---
+++ DiceGameMidterm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceGameMidterm
+//
+//  Created by Sabri Sönmez on 11/13/19.
+//  Copyright © 2019 Sabri Sönmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
